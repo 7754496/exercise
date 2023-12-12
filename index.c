03b18e16367b1dec89f9d8a18f41c18de98643cd
keyframes <68995302+7754496@users.noreply.github.com>
2023-12-13 00:14:36 +0800

    添加项目文件。

---
+++ index.c
@@ -0,0 +1,36 @@
+﻿
+/*
+两个正整数的最大公约数是能够整除这两个整数的最大整数。
+请按如下函数原型编写计算最大公约数的函数Gcd()。
+int Gcd(int a, int b);
+在主函数中调用该函数计算，并输出从键盘任意输入的两整数的最大公约数。
+**要求输入提示信息为: "Input a,b:\n"
+**要求输入格式为: "%d,%d"
+**要求输出格式为
+如果a或b不是正整数，输出: "Input number should be positive!\n"
+否则输出: "Greatest Common Divisor of %d and %d is %d\n"
+
+Input a,b:↙
+16,8
+Greatest Common Divisor of 16 and 8 is 8↙
+
+Input a,b:↙
+16,0
+Input number should be positive!↙
+
+Input a,b:↙
+-5,3
+Input number should be positive!↙
+
+*/
+#include <stdio.h>
+#include <math.h>
+
+
+int main()
+{
+
+
+	return 0;
+}
+
